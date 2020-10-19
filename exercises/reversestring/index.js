565eef0f4e09121fc62001a33ecad033e306c6b5
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2020-10-18 22:54:44 -0400

    1

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+   return str.split('').reverse().join('')
+}
+
+function reverse2(str) {
+    let reversed = '';
+    for (const char of str) {
+        reversed = char + reversed;
+    }
+    console.log(reversed)
+
+ }
+
+function reverse3(str) {
+   str.split('').reduce((reversed,character) => {
+        return character + reversed 
+   },'')
+ } 
+console.log( reverse3("Hello"))
+
 
 module.exports = reverse;
