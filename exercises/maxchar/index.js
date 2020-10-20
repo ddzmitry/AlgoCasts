c0dd8b4c90988eb984af1803dbaf2d45c17f7397
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2020-10-19 22:58:27 -0400

    3

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+    let a = str.toString().split("").reduce((obj,char) => {
+        // Key {a : 1 ,b:1,c:7}
+        if(!obj[char]){
+            obj[char] =1     
+        } else {
+            obj[char] ++
+       
+        }
+        return obj
+    },{})
+    
+     // Key {a : 1 ,b:1,c:7}
+     // keys [a,b,c,d]
+     return Object.keys(a).reduce((x,y)=> {
+        // if a[c] > a[b] return c else return b
+        if(a[x] > a[y]){
+            return x
+        } else {
+            return y
+        }
+    })
+
+}
 
 module.exports = maxChar;
