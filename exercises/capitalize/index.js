5ca7ea383f3f8837eaeadb5dc16cbd3c4a14fbaf
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2020-10-21 09:03:57 -0400

    Solutions

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -6,7 +6,26 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
+https://www.amazon.jobs/en/jobs/1286086/software-development-engineer
+https://www.amazon.jobs/en/jobs/1179795/software-development-engineer
+https://www.amazon.jobs/en/jobs/1225322/systems-software-engineer
+https://www.amazon.jobs/en/jobs/1299832/software-development-engineer
+function capitalize(str) {
 
-function capitalize(str) {}
+    let temp =  str.split(' ').reduce((reducer,value) => { 
+        return reducer += `${value.charAt(0).toUpperCase().concat(value.slice(1,value.length))} `
+     },"")
+
+     return temp.slice(0,temp.length -1)
+}
+
+function capitalize2(str) {
+const words = []
+for (let word of str.split(' ')){
+    words.push(word[0].toUpperCase() + word.slice(1))
+}
+return words.join(' ')
+}
+
 
 module.exports = capitalize;
