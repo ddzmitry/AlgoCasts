ade97762cc4d54e6645611529a21383a94edf683
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2020-10-20 21:04:56 -0400

    3

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,33 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+let chunked = []
+let chunkIndex = 0;
+let counter = 0
+array.map((x,y) => {
+    // check if index exists 
+    if(!chunked[chunkIndex]){
+        // if not set it to array with calue
+        chunked[chunkIndex] = [x]
+    } else {
+        // just add value to array
+        chunked[chunkIndex] = [...chunked[chunkIndex],...[x]]
+    }
+    // increase counter
+    counter ++ 
+
+    // if counter eq size , move on to next and increase index
+    if(counter == size ){
+        counter = 0
+        chunkIndex ++
+    }
+    
+})
+return chunked
+}
+
+function chunk2(array, size) {}
 
 module.exports = chunk;
