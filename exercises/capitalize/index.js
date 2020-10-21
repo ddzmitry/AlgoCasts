47449d169ae3a5ba799c3a85db4311b13fca84e8
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2020-10-21 19:33:02 -0400

    Solutions

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -27,5 +27,18 @@ for (let word of str.split(' ')){
 return words.join(' ')
 }
 
+function capitalize3(str) {
+
+    let result = str[0].toUpperCase();
+
+    for (let i=1; i < str.length; i++) {
+        if(str[i -1] === ' '){
+            result += str[i].toUpperCase();
+        }
+    }
+    return result;
+
+}
+    
 
 module.exports = capitalize;
